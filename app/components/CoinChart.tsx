9b160276d2639f3b9c969a1664c60feee1fccda4
zahoor248 <zahoor.alphasquad@gmail.com>
2024-09-19 19:52:46 +0500

    feat(CSV Editor)

--- app/components/CoinChart.tsx
+++ app/components/CoinChart.tsx
@@ -1,110 +1,104 @@
 "use client";
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+import { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
 
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "./ui/card";
-import {
-  ChartConfig,
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "./ui/chart";
-
-import { useState, useEffect, useRef } from "react";
-import useWebSocket from "react-use-websocket";
-
-export const description = "A linear line chart";
-
-const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
-  },
-} satisfies ChartConfig;
-
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog";
+import { CSVLink, CSVDownload } from "react-csv";
 export function CoinChart() {
-  const [chartData, setChartData] = useState([]);
-  const WS_URL = "wss://wspap.okx.com:8443/ws/v5/public";
-  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
-    WS_URL,
-    {
-      share: false,
-      shouldReconnect: () => true,
-    }
-  );
+  const [csvData, setCSVData] = useState({ data: [], key: 0 });
+  const [editData, setEditData] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
 
-  useEffect(() => {
-    if (readyState) {
-      sendJsonMessage({
-        op: "subscribe",
-        args: [
-          {
-            channel: "tickers",
-            instId: "BTC-USD-SWAP",
-          },
-        ],
-      });
-    }
-  }, [readyState]);
+    Papa.parse(file, {
+      complete: function (results) {
+        setCSVData(results.data);
+        console.log(results);
+      },
+    });
+  };
 
-  useEffect(() => {
-    if (lastJsonMessage && lastJsonMessage?.data) {
-      const newPrice = lastJsonMessage.data[0]?.last;
-      if (newPrice) {
-        setChartData((prevData) => {
-          const newData = {
-            time: new Date().toLocaleTimeString(),
-            price: parseFloat(newPrice),
-            yaxisPrice: parseFloat(newPrice),
-          };
-          return [...prevData, newData];
-        });
-      }
-    }
-  }, [lastJsonMessage]);
+  const handleChange = (data, key, rowkey) => {
+    console.log(data, key, rowkey);
+    console.log(csvData[rowkey][key]);
+    csvData[rowkey][key] = data;
+    setCSVData(csvData);
+  };
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>BTC Bitcoin Price</CardTitle>
-        <CardDescription>Real-time updates</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <ChartContainer config={chartConfig}>
-          <LineChart data={chartData} margin={{ left: 12, right: 12 }}>
-            <CartesianGrid vertical={true} />
-            <YAxis
-              dataKey="yaxisPrice"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              domain={["dataMin", "dataMax"]}
-            />
-            <XAxis
-              dataKey="time"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-            />
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
-            <Line
-              dataKey="price"
-              type="linear"
-              stroke="green"
-              strokeWidth={2}
-              dot={true}
-            />
-          </LineChart>
-        </ChartContainer>
-      </CardContent>
-    </Card>
+    <div className="max-w-[900px] m-auto w-full flex flex-col gap-8">
+      <form className="flex gap-2">
+        <input
+          type="file"
+          accept=".csv"
+          onChange={handleSubmit}
+          placeholder="upload a csv"
+          className="border"
+        />
+      </form>
+      <div>
+        {csvData && csvData?.length > 0 && (
+          <CSVLink data={JSON.stringify(csvData)} headers={csvData[0]}>
+            Download me
+          </CSVLink>
+        )}
+
+        <Table className=" border">
+          <TableBody>
+            {csvData &&
+              csvData?.length &&
+              csvData?.map((item, key) => (
+                <TableRow>
+                  {csvData &&
+                    csvData[key]?.length &&
+                    csvData[key]?.map((data, key) => (
+                      <TableCell className="font-medium">
+                        {data || "N/A"}
+                      </TableCell>
+                    ))}
+                  <TableCell className="cursor-pointer">
+                    <Dialog>
+                      <DialogTrigger
+                        onClick={() => setEditData({ data: item, key: key })}
+                      >
+                        Edit
+                      </DialogTrigger>
+                      <DialogContent>
+                        <DialogTitle>Edit your csv</DialogTitle>
+                        {editData?.data?.map((item, key) => (
+                          <input
+                            type="text"
+                            defaultValue={item}
+                            className="border py-3 w-full px-4"
+                            onChange={(e) =>
+                              handleChange(e.target.value, key, editData.key)
+                            }
+                          />
+                        ))}
+                      </DialogContent>
+                    </Dialog>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
   );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.0/papaparse.min.js"></script>
         <Meta />
         <Links />
       </head>
